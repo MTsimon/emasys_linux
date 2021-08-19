2e1a4985bfe8ce331140e440dde1c25543db0c65
Simon Juul Møller <simon@micro-technic.com>
2021-08-19 16:11:03 +0300

    script and documentation

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # emasys_linux
+
+
+## Guide til hvordan linuxkerne og driver til EW-7811Un-V2 bygges på Windows maskine
+
+* Installer GIT https://git-scm.com/downloads
+* Installer docker: https://www.docker.com/products/docker-desktop
+
+* Kør følgende kommandoer i kommandoprompten (cmd) 
+```console
+C:\Users\simon\Desktop>git config --global core.autocrlf input
+
+C:\Users\simon\Desktop>git clone https://github.com/MTsimon/emazys_linux.git
+
+C:\Users\simon\Desktop>cd emazys_linux
+
+C:\Users\simon\Desktop\emasys_linux>docker build -t emazys_linux .
+
+C:\Users\simon\Desktop\emasys_linux>docker run -it --name=emazys_container -v <local_dir>:/mnt/host emazys_linux
+```
+> Eksempel på <local_dir> kan være C:\Users\%USERNAME%\Desktop , der oprettes en ny mappe der hedder "release", some indeholder zImage og 8188eu.ko på det lokale skrivebord. Husk at fjerne "<>" tegn.
+```console
+root@8c98670601ec:/# make_kernel
+```
+> Dette kan tage lang tid afhængig af internetforbindelse og CPU-hastighed
+Exit
+
+Kernefilerne kan nu findes i <local_dir>/rootfs mappen.
+
+wpa_supplicant_8_jb_4.2_rtw_r25670.20171213 skal bruges til at oprette accesspoint. Kan nemmest kompileres på target.
--- make_kernel.sh
+++ make_kernel.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 KERNEL_DIR=/emazys/linux
-OUTDIR=/emazys/release
+OUTDIR=/mnt/host/rootfs
 
 echo "Welcome to EmaZys toolchain. We are now ready to build the linux Kernel"
 
 cd /emazys
-mkdir release
 git clone https://github.com/beagleboard/linux.git ${KERNEL_DIR}
 cd linux
 
@@ -17,11 +16,17 @@ echo "Patch applied"
 cp /emazys/config-3.14.35-ti-r56 ${KERNEL_DIR}/.config
 echo "Compiling kernel"
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j8
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- LOADADDR=0x82000000 uImage dtbs
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j8 modules
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=${OUTDIR} modules_install
+make headers_install ARCH=arm INSTALL_HDR_PATH=${OUTDIR}/usr
 echo "Compiling driver for RTL8188EUS (EW-7811Un-V2)"¨
 cd /emazys/rtl8188EUS_linux_v5.7.6.1_36803.20200508
 make modules
 echo "Finnished compiling"
-cp ${KERNEL_DIR}/arch/arm/boot/zImage ${OUTDIR}
+
+mkdir -p ${OUTDIR}
+cp -a ${KERNEL_DIR}/arch/arm/boot ${OUTDIR}
+cp /emazys/rtl8188EUS_linux_v5.7.6.1_36803.20200508/8188eu.ko ${OUTDIR}/lib/modules/3.14.35+/kernel/drivers/net/wireless
 cp /emazys/rtl8188EUS_linux_v5.7.6.1_36803.20200508/8188eu.ko ${OUTDIR}
-echo "zImage and 8188eu.ko can be found in the release directory"
+echo "zImage and 8188eu.ko can be found in the rootfs directory"
